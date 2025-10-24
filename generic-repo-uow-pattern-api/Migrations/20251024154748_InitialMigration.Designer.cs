ad5a78e0c8280fd1e399a5b7e1016198d5a42407
Horla96 <sexysulai@gmail.com>
2025-10-24 16:57:22 +0100

    Implement Unit of Work Pattern and Enhance Generic Repository

--- generic-repo-uow-pattern-api/Migrations/20251024154748_InitialMigration.Designer.cs
+++ generic-repo-uow-pattern-api/Migrations/20251024154748_InitialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using generic_repo_pattern_api.Data;
 
@@ -11,9 +12,11 @@ using generic_repo_pattern_api.Data;
 namespace generic_repo_pattern_api.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251024154748_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
