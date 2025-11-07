cca53073910950352cc33be6083c948369d0999f
Horla96 <sexysulai@gmail.com>
2025-11-07 10:22:50 +0100

    Implement AutoMapper in ASP.NET Core Web API for Simplified Entity-DTO Mapping and CRUD Operations

---
+++ generic-repo-uow-pattern-api/MapperProfile/YourMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using generic_repo_pattern_api.Entity;
+using generic_repo_pattern_api.ViewModel;
+
+namespace generic_repo_pattern_api.MapperProfile
+{
+    public class YourMappingProfile : Profile
+    {
+        public YourMappingProfile()
+        {
+            //CreateMap<entity, Dto>();
+            CreateMap<Product, ProductRequest>().ReverseMap();
+            //CreateMap<Order, OrderRequest>().ReverseMap();
+
+            //CreateMap<Blog, BlogPostReqeust>().ReverseMap();
+            //CreateMap<Post, PostRequest>().ReverseMap();
+            //CreateMap<Post, PostResponse>().ReverseMap();
+        }
+    }
+}
